d88e4ec2a92b70ecbebc8887eb942d5f08af741a
viniciusfcar <38993929+viniciusfcar@users.noreply.github.com>
2019-12-07 14:08:40 -0300

    UPDATE

--- src/main/java/net/milkpoint/api/LoginResource.java
+++ src/main/java/net/milkpoint/api/LoginResource.java
@@ -20,8 +20,8 @@ public class LoginResource {
 	@Autowired
 	LoginService service;
 	
-	@GetMapping("/login")
-	public ResponseEntity<Object> login(String email, String senha) {
+	@PostMapping("/login")
+	public ResponseEntity<Object> login(@RequestBody @Valid String email, String senha) {
 		if(email != null && senha != null) {
 			Object obj = service.login(email, senha);
 			
